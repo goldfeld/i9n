401f80b99aeedd6db1db528d6fae3f2c3c67b2a1
Vic Goldfeld <vic@longstorm.org>
2014-07-28 06:18:57 -0300

    remove lein-git-deps and get claude from clojars

--- project.clj
+++ project.clj
@@ -8,10 +8,10 @@
   :aliases {"auto" ["do" "clean," "cljsbuild" "auto"]}
   :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2014"]]
+                 [org.clojure/clojurescript "0.0-2014"]
+                 [longstorm/claude "0.1.4"]]
   :cljsbuild {:builds [{:id "demo"
-                        :source-paths ["src" "demo"
-                                       ".lein-git-deps/claude/src"]
+                        :source-paths ["src" "demo"]
                         :compiler {:target :nodejs
                                    :output-to "resources/public/demo.js"
                                    :optimizations :simple}}]}
@@ -20,7 +20,5 @@
                         [org.bodil/redlobster "0.2.1"]]
          :hooks [leiningen.cljsbuild]
          :plugins [[lein-cljsbuild "1.0.0-alpha2"]
-                   [lein-git-deps "0.0.1-SNAPSHOT"]
                    [lein-npm "0.4.0"]]
-         :git-dependencies [["https://github.com/longstorm/claude.git"]]
          :node-dependencies [[blessed "0.0.29"]]}})
