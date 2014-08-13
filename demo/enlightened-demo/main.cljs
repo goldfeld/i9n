408c3f3c2d311b806be4035dbe43b2d5bfcbdc6b
Vic Goldfeld <vic@longstorm.org>
2014-08-13 05:01:26 -0300

    update demo with async and text viewer et al

--- demo/enlightened-demo/main.cljs
+++ demo/enlightened-demo/main.cljs
@@ -1,17 +1,35 @@
 (ns enlightened-demo.main
   (:require [cljs.nodejs :as node]
-            [enlightened.core :refer [get-screen]]
-            [enlightened.widgets :as enlightened :refer [menu-view]]))
+            [cljs.core.async :as a]
+            [clojure.string :as strng]
+            [claude.process :as proc]
+            [enlightened.core :as core]
+            [enlightened.navigation :refer [navigation-view]]))
 
-(defn exit
-  ([] (exit 0))
-  ([code] (.exit node/process code)))
+(defn sad-async []
+  (let [chan (a/chan)]
+    (a/put! chan "Hop!")
+    (a/put! chan "haha")
+    (a/put! chan :hey)
+    (a/put! chan "hooooo")
+    (a/put! chan :ho)
+    chan))
 
-(defn -main [& args]
-  (doto (get-screen)
-    (.key (clj->js ["escape" "C-c"]) #(exit)))
-  (menu-view [["my-menu" ["do" :do "think" :think]]
-              [:do "focus" ["hey" :hey]]]
-             identity []))
+(defn nav []
+  [["inkstick" ["view text" :Text
+                "test" :test
+                "quit" #(proc/exit)]]
+   [:Text "Text" (str "Lorem ipsum dolores siamet"
+                      (strng/join (repeat 888 " lorem ipsum dolores siamet"))
+                      ". Lorem ipsum dolores siamet.")]
+   [:test "focus" ["sadly asynchronous" #(sad-async)
+                   "sysiphus" #(navigation-view (nav) identity {})
+                   "hey" :hey]]
+   [:hey "guess" ["a" :a "b" :b "c" :c]]
+   [:ho "go" ["hum" (constantly "sad")]]])
+
+(defn -main [& input]
+  (core/bind-global-keys ["C-c" "q"] #(proc/exit))
+  (navigation-view (nav) identity []))
 
 (set! *main-cli-fn* -main)
