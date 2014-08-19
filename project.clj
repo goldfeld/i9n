52cc6a385c280f8553a56506f27fca6acc40dc93
Vic Goldfeld <vic@longstorm.org>
2014-08-19 14:31:17 -0300

    enlightened 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject longstorm/enlightened "0.2.0"
+(defproject longstorm/enlightened "0.2.1"
   :description "Fast interactive terminal (curses) UIs with cljs and nodejs."
   :url "https://github.com/longstorm/enlightened"
   :license {:name "Eclipse Public License"
