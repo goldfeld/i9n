13453c9f2b9b1070723c8af0dc5d3f87066779e0
Vic Goldfeld <vic@longstorm.org>
2014-09-22 22:31:40 -0300

    move :state to multimethod operation

---
+++ src/lib/i9n/op/state.cljs
@@ -0,0 +1,32 @@
+(ns i9n.op.state
+  (:require [i9n.nav-entry :as nav-entry]
+            [i9n.ext :refer [i9n-op]]))
+
+(defn update-state [nav state-id state-val]
+  (if-let [{:keys [set deps]} (get-in nav [:state state-id])]
+    (update-in nav [:state state-id :val]
+               (fn [old-val]
+                 (set state-val old-val 
+                      (select-keys (:state nav) deps))))
+    (assoc-in nav [:state state-id]
+              {:val state-val
+               :get (fn [val] val)
+               :set (fn [val _ _] val)})))
+
+(defn update-states [nav & id-state-pairs]
+  (reduce (fn [n [state-id state-val]]
+            (if-let [dpdnts (get-in n [:state state-id :dependents])]
+              (reduce (fn [n' dpdt]
+                        (condp apply [dpdt]
+                          fn? (do (dpdt) n')
+                          keyword? (let [d (get-in n' [:state dpdt])]
+                                     (or (and (map? d) (= :eager (:computed d))
+                                              (update-state n' dpdt (:val d)))
+                                         n'))))
+                      (update-state n state-id state-val)
+                      dpdnts)
+              (update-state n state-id state-val)))
+          nav (partition 2 id-state-pairs)))
+
+(defmethod i9n-op :state [[cmd & args] nav more]
+  (apply update-states nav args))
--- src/node/i9n/os/navigation.cljs
+++ src/node/i9n/os/navigation.cljs
@@ -141,32 +141,6 @@
               :left ["h" "left"]
               :right ["l" "right"]}})
 
-(defn update-state [nav state-id state-val]
-  (if-let [{:keys [set deps]} (get-in nav [:state state-id])]
-    (update-in nav [:state state-id :val]
-               (fn [old-val]
-                 (set state-val old-val 
-                      (select-keys (:state nav) deps))))
-    (assoc-in nav [:state state-id]
-              {:val state-val
-               :get (fn [val] val)
-               :set (fn [val _ _] val)})))
-
-(defn update-states [nav & id-state-pairs]
-  (reduce (fn [n [state-id state-val]]
-            (if-let [dpdnts (get-in n [:state state-id :dependants])]
-              (reduce (fn [n' dpdt]
-                        (condp apply [dpdt]
-                          fn? (do (dpdt) n')
-                          keyword? (let [d (get-in n' [:state dpdt])]
-                                     (or (and (map? d) (= :eager (:computed d))
-                                              (update-state n' dpdt (:val d)))
-                                         n'))))
-                      (update-state n state-id state-val)
-                      dpdnts)
-              (update-state n state-id state-val)))
-          nav (partition 2 id-state-pairs)))
-
 (defn create-pane
   ([current initial-nav widget]
      (create-pane current initial-nav widget config-default))
@@ -188,7 +162,6 @@
        (a/reduce
         (fn [nav [cmd & args :as op]]
           (case cmd
-            :state (apply update-states nav args)
             :i9n (do (ext/custom-i9n (first args)
                                      {:parent widget :nav nav :cfg cfg})
                      nav)
