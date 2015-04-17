d24289f417328f6df9094badf64c9bae961ef8ef
Vic Goldfeld <vic@longstorm.org>
2015-04-17 10:23:18 -0300

    i9n 0.2.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject i9n "0.2.12"
+(defproject i9n "0.2.13"
   :description "Fast declarative terminal (curses) UIs with cljs and nodejs."
   :url "https://github.com/goldfeld/i9n"
   :license {:name "Eclipse Public License"
